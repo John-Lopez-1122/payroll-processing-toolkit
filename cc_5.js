9b7ff0febb88a4720b92d320baf8593907a123db
John-Lopez-1122 <jlopez354@usf.edu>
2026-02-22 11:06:32 -0500

    First functions created, returns values correctly

--- cc_5.js
+++ cc_5.js
@@ -1,5 +1,33 @@
 let employee = [ // [name, hourlyRate, hoursWorked]
-    {Name: "Twilight Sparkle", hourlyRate: 84, hoursWorked: 65},
-    {Name: "Spike", hourlyRate: 84, hoursWorked: 26},
-    {Name: "Owlowiscious", hourlyRate: 84, hoursWorked: 40},
+    {name: "Twilight Sparkle", hourlyRate: 102, hoursWorked: 65},
+    {name: "Spike", hourlyRate: 84, hoursWorked: 26},
+    {name: "Owlowiscious", hourlyRate: 68, hoursWorked: 40}
 ]
+
+
+function employeeList(write) {
+    employee.forEach(item => {
+        const name = item.name
+        const rate = item.hourlyRate
+        const hours = item.hoursWorked
+
+        console.log(`Name: ${name} | Rate: ${rate} | Hours: ${hours}`)
+    }
+    )
+}
+
+employeeList(employee)
+
+
+function calulateBasePay(rate, hours) {
+    for (job of employee) {
+    let restrictedHours = job.hoursWorked
+    if (restrictedHours > 40)
+        restrictedHours = 40
+    let basePay = restrictedHours * job.hourlyRate
+    console.log(basePay)
+    }
+}
+
+
+calulateBasePay(employee.hourlyRate, employee.hoursWorked)
