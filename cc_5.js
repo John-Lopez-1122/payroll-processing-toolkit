21dfae52444706b9d1caf4ece4a18ff837a35b9d
John-Lopez-1122 <jlopez354@usf.edu>
2026-02-22 10:05:07 -0500

    first commit

---
+++ cc_5.js
@@ -0,0 +1,5 @@
+let employee = [ // [name, hourlyRate, hoursWorked]
+    {Name: "Twilight Sparkle", hourlyRate: 84, hoursWorked: 65},
+    {Name: "Spike", hourlyRate: 84, hoursWorked: 26},
+    {Name: "Owlowiscious", hourlyRate: 84, hoursWorked: 40},
+]
